021e762a27e0007bf20f6d09c5d8ab321285c7b5
Konstantin <n1kron92@gmail.com>
2018-09-11 17:11:47 +0300

    add new onboarding + cloack

--- IslamBook/AppDelegate.swift
+++ IslamBook/AppDelegate.swift
@@ -13,36 +13,43 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
+    let configUrl = "http://disalwow.site/api/check-clean"
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        
-        if !UserDefaults.standard.bool(forKey: "didSee") {
-            UserDefaults.standard.set(true, forKey: "didSee")
-            
-            let storyboard = UIStoryboard(name: "Main", bundle: nil)
-            let viewController = storyboard.instantiateViewController(withIdentifier: "OnboardingViewController")
-            self.window?.rootViewController = viewController
-            self.window?.makeKeyAndVisible()
+        loadConfig()
+        if UserDefaults.standard.object(forKey: "OnOnboarding") as? Int == 1 {
+            let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+            self.window?.rootViewController = storyboard.instantiateViewController(withIdentifier: "NavigationVC")
         }
         return true
     }
-
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    
+    func loadConfig() {
+        var request = URLRequest(url: URL(string: configUrl)!)
+        request.httpMethod = "POST"
+        request.addValue("application/json" , forHTTPHeaderField: "Content-Type")
+        let bundleID = Bundle.main.bundleIdentifier
+        let params = ["package": bundleID]
+        let postData = try! JSONSerialization.data(withJSONObject: params, options: .prettyPrinted)
+        request.httpBody = postData
+        let userDefaults = UserDefaults.standard
+        URLSession.shared.dataTask(with: request) { (data, _, _) in
+            do {
+                guard let data = data,
+                    let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any],
+                    let show = json["can_show"] as? Bool,
+                    show == true else {
+                        userDefaults.set(false , forKey: "check")
+                        userDefaults.synchronize()
+                        return
+                }
+                userDefaults.set(true , forKey: "check")
+                userDefaults.synchronize()
+            } catch {
+                userDefaults.set(false , forKey: "check")
+                userDefaults.synchronize()
+            }
+            }.resume()
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
